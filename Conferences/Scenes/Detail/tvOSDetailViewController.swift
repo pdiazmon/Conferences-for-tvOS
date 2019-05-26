283497f8e6d434c228426899ea398453f6a5348f
pdiazmon <pedroluis.diaz@gmail.com>
2019-05-26 20:57:34 +0200

    Buttons added in detail view for play, watchlist and watched

--- Conferences/Scenes/Detail/tvOSDetailViewController.swift
+++ Conferences/Scenes/Detail/tvOSDetailViewController.swift
@@ -18,6 +18,10 @@ class tvOSDetailViewController: UIViewController {
     private weak var thumbnailImageDownloadOperation: Operation?
     private var PROFILEIMG_HEIGHTWIDTH: CGFloat = 0
     private var THUMB_WIDTH: CGFloat = 0
+    private let BUTTON_HEIGHTWIDTH: CGFloat = 70
+    private let BUTTON_IMAGESIZE: CGFloat = 50
+    private let BUTTON_CORNERRADIUS: CGFloat = 10
+    private let BUTTON_EDGEINSET: CGFloat = 7
 
     private lazy var talkTitle: UILabel = {
         var title = UILabel()
@@ -158,19 +162,67 @@ class tvOSDetailViewController: UIViewController {
         return line
     }()
     
-    private var buttonParallaxEffectOptions: ParallaxEffectOptions?
-    
-    var playButton: UIButton = {
+    private lazy var playButton: UIButton = {
         var button = UIButton()
-
-        button.setTitle("Play", for: .normal)
+        
+        button.tintColor          = .white
+        button.layer.cornerRadius = BUTTON_CORNERRADIUS
+        button.layer.borderColor  = UIColor.white.cgColor
+        button.imageEdgeInsets    = UIEdgeInsets(top: BUTTON_EDGEINSET, left: BUTTON_EDGEINSET, bottom: BUTTON_EDGEINSET, right: BUTTON_EDGEINSET)
+        button.height(BUTTON_HEIGHTWIDTH)
+        button.width(BUTTON_HEIGHTWIDTH)
         button.addTarget(self, action: #selector(didSelectPlay), for: UIControl.Event.primaryActionTriggered)
-        button.layer.cornerRadius = 15.0
-        button.clipsToBounds      = true
-        button.contentEdgeInsets  = UIEdgeInsets(top: 20, left: 40, bottom: 20, right: 40)
-        button.titleLabel?.font   = .systemFont(ofSize: 35, weight: .medium)
         
         return button
+    }()
+
+    
+    private lazy var watchlistButton: UIButton = {
+        var button = UIButton()
+        
+        button.tintColor          = .white
+        button.layer.cornerRadius = BUTTON_CORNERRADIUS
+        button.layer.borderColor  = UIColor.white.cgColor
+        button.imageEdgeInsets    = UIEdgeInsets(top: BUTTON_EDGEINSET, left: BUTTON_EDGEINSET, bottom: BUTTON_EDGEINSET, right: BUTTON_EDGEINSET)
+        button.height(BUTTON_HEIGHTWIDTH)
+        button.width(BUTTON_HEIGHTWIDTH)
+        button.addTarget(self, action: #selector(didSelectWatchlist), for: UIControl.Event.primaryActionTriggered)
+        
+        return button
+    }()
+
+    private lazy var watchButton: UIButton = {
+        var button = UIButton()
+        
+        button.tintColor          = .white
+        button.layer.cornerRadius = BUTTON_CORNERRADIUS
+        button.layer.borderColor  = UIColor.white.cgColor
+        button.imageEdgeInsets    = UIEdgeInsets(top: BUTTON_EDGEINSET, left: BUTTON_EDGEINSET, bottom: BUTTON_EDGEINSET, right: BUTTON_EDGEINSET)
+        button.height(BUTTON_HEIGHTWIDTH)
+        button.width(BUTTON_HEIGHTWIDTH)
+        button.addTarget(self, action: #selector(didSelectWatch), for: UIControl.Event.primaryActionTriggered)
+        
+        return button
+    }()
+
+    private lazy var stackButtons: UIStackView = {
+        var stack = UIStackView(arrangedSubviews: [playButton, watchlistButton, watchButton])
+        
+        stack.axis      = .horizontal
+        stack.spacing   = 50
+        stack.alignment = .center
+        
+        return stack
+    }()
+    
+    private lazy var tooltipLabel: UILabel = {
+        var label = UILabel()
+        
+        label.font          = .systemFont(ofSize: 25, weight: .medium)
+        label.textColor     = .primaryText
+        label.textAlignment = .center
+
+        return label
     }()
     
     override func viewDidLoad() {
@@ -186,8 +238,6 @@ class tvOSDetailViewController: UIViewController {
         talkDetails.sizeToFit()
         profileName.sizeToFit()
         profileAbout.sizeToFit()
-        
-        setParallaxEffect()
     }
     
     private func setup() {
@@ -197,7 +247,8 @@ class tvOSDetailViewController: UIViewController {
         self.view.addSubview(line1)
         self.view.addSubview(stackSpeaker)
         self.view.addSubview(line2)
-        self.view.addSubview(playButton)
+        self.view.addSubview(stackButtons)
+        self.view.addSubview(tooltipLabel)
         
         self.view.backgroundColor = .panelBackground
     }
@@ -221,8 +272,12 @@ class tvOSDetailViewController: UIViewController {
         self.line2.leading(to: line1)
         self.line2.trailing(to: line1)
         
-        self.playButton.topToBottom(of: line2, offset: 30)
-        self.playButton.centerXToSuperview()
+        self.stackButtons.topToBottom(of: line2, offset: 30)
+        self.stackButtons.leading(to: line2)
+        
+        self.tooltipLabel.bottomToSuperview(offset: -10)
+        self.tooltipLabel.centerXToSuperview()
+        self.tooltipLabel.widthToSuperview()
     }
     
     func render(talk: TalkModel) {
@@ -236,6 +291,10 @@ class tvOSDetailViewController: UIViewController {
         self.profileName.text    = talk.speaker.firstname + " " + talk.speaker.lastname
         self.twitterAccount.text = "@" + (talk.speaker.twitter ?? "")
         self.profileAbout.text   = talk.speaker.about
+        
+        playButton.setImage(getButtonImage(named: "play"), for: .normal)
+        watchlistButton.setImage(watchlistImage(), for: .normal)
+        watchButton.setImage(watchImage(), for: .normal)
       
         guard let profileImageUrl = URL(string: talk.speaker.image) else { return }
         self.profileImageDownloadOperation?.cancel()
@@ -265,67 +324,117 @@ class tvOSDetailViewController: UIViewController {
 
 }
 
-extension tvOSDetailViewController {
-
-    @objc func didSelectPlay() {
-        guard let talk = self.currentTalk else { return }
-        
-      self.coordinator?.playTalk(talk: talk)
-    }
-}
-
 // MARK: - ParallaxEffect
 
 extension tvOSDetailViewController {
 
     override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
         coordinator.addCoordinatedAnimations({ [unowned self] in
-            // Add parallax effect only to controls inside this view controller
-            if let nextFocusedView = context.nextFocusedView , nextFocusedView.isDescendant(of: self.view) {
-                switch context.nextFocusedView {
-                case is UIButton:
-                    // Custom parallax effect for the button
-                    var buttonParallaxEffectOptions = self.buttonParallaxEffectOptions!
-                    self.playButton.addParallaxMotionEffects(with: &buttonParallaxEffectOptions)
-                default:
-                    // For the anyView use default options
-                    context.nextFocusedView?.addParallaxMotionEffects()
+            // Remove parallax effect for the view that lost focus
+            if let prev = (context.previouslyFocusedItem as? UIButton) {
+                if (prev.isDescendant(of: self.stackButtons)) {
+                    prev.removeParallaxMotionEffects()
+                    prev.layer.borderWidth = 0
+                    self.removeButtonTooltip()
                 }
             }
 
-            // Remove parallax effect for the view that lost focus
-            switch context.previouslyFocusedView {
-            case is UIButton:
-                // Because anyButton uses custom glow container we have to pass it to remove parallax effect correctly
-                self.playButton.removeParallaxMotionEffects(with: self.buttonParallaxEffectOptions!)
-            default:
-                context.previouslyFocusedView?.removeParallaxMotionEffects()
+            if let next = context.nextFocusedView as? UIButton {
+                if (next.isDescendant(of: self.stackButtons)) {
+                    next.addParallaxMotionEffects()
+                    next.layer.borderWidth = 3
+                    self.setButtonTooltip(for: next)
+                }
             }
 
-            }, completion: nil)
+        }, completion: nil)
     }
-    
-    private func setParallaxEffect() {
-        // Define custom glow for the parallax effect
-        let customGlowContainer = UIView(frame: playButton.bounds)
-        customGlowContainer.clipsToBounds = true
-        customGlowContainer.backgroundColor = UIColor.clear
-        playButton.subviews.first?.subviews.last?.addSubview(customGlowContainer)
+}
 
-        buttonParallaxEffectOptions = ParallaxEffectOptions(glowContainerView: customGlowContainer)
+
+// MARK: - Buttons functions
+
+extension tvOSDetailViewController {
+    
+    @objc func didSelectPlay() {
+        guard let talk = self.currentTalk else { return }
         
-        // Add gray background color to make glow effect be more visible
-        playButton.setBackgroundImage(getImageWithColor(UIColor.lightGray, size: playButton.bounds.size), for: UIControl.State())
+        self.coordinator?.playTalk(talk: talk)
     }
-
     
-    func getImageWithColor(_ color: UIColor, size: CGSize) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        color.setFill()
-        UIRectFill(rect)
-        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return image
+    @objc private func didSelectWatchlist() {
+        guard var talk = self.currentTalk else { return }
+        
+        talk.onWatchlist.toggle()
+        
+        watchlistButton.setImage(watchlistImage(), for: .normal)
+        setButtonTooltip(for: watchlistButton)
+        
+        var tag = TagModel(title: "Watchlist", query: "realm_watchlist", isActive: talk.onWatchlist)
+        TagSyncService.shared.handleStoredTag(&tag)
+    }
+    
+    @objc private func didSelectWatch() {
+        guard var talk = self.currentTalk else { return }
+        
+        talk.watched.toggle()
+        
+        watchButton.setImage(watchImage(), for: .normal)
+        setButtonTooltip(for: watchButton)
+        
+        var tag = TagModel(title: "Confinue watching", query: "realm_continue", isActive: false)
+        TagSyncService.shared.handleStoredTag(&tag)
+        
+        self.coordinator?.reloadCollection()
+    }
+    
+    private func watchlistImage() -> UIImage? {
+        guard var talk = self.currentTalk else { return nil }
+        
+        return talk.onWatchlist ? getButtonImage(named: "watchlist_filled") : getButtonImage(named: "watchlist")
+    }
+    
+    private func watchImage() -> UIImage? {
+        guard var talk = self.currentTalk else { return nil }
+        
+        return talk.watched ? getButtonImage(named: "watch_filled") : getButtonImage(named: "watch")
+    }
+    
+    private func getButtonImage(named: String) -> UIImage? {
+        return UIImage(named: named)?.resized(to: BUTTON_IMAGESIZE)
+    }
+    
+    private func setButtonTooltip(for button: UIButton) {
+        var tooltip: String = ""
+        
+        if (button == self.playButton) {
+            tooltip = "Play talk"
+        }
+        else if (button == self.watchButton) {
+            if (self.currentTalk?.watched ?? false) {
+                tooltip = "Set talk to unwatched"
+            }
+            else {
+                tooltip = "Set talk to watched"
+            }
+        }
+        else if (button == self.watchlistButton) {
+            if (self.currentTalk?.onWatchlist ?? false) {
+                tooltip = "Remove talk from watchlist"
+            }
+            else {
+                tooltip = "Add talk to watchlist"
+            }
+        }
+        
+        DispatchQueue.main.async {
+            self.tooltipLabel.text = tooltip
+        }
+    }
+    
+    private func removeButtonTooltip() {
+        DispatchQueue.main.async {
+            self.tooltipLabel.text = ""
+        }
     }
 }
